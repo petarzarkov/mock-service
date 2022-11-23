60dea0b3446da6d919dc060a0dd3aa043eedfe69
Petar Zarkov <p.zarkov@draftkings.com>
2022-11-23 15:57:30 +0200

    CAS-37063 support-array-on-stub-creation and store log only if stub exists

--- src/handlers/api/stub/stub.ts
+++ src/handlers/api/stub/stub.ts
@@ -4,18 +4,20 @@ import { withResult } from "@utils";
 import { Stub } from "@contracts";
 
 export const stub = (action: "create" | "getAll" | "delete") => async (
-    req: FastifyRequest<{ Params: { id?: string }; Body: Stub }, Server, IncomingMessage>,
+    req: FastifyRequest<{ Params: { id?: string }; Body: Stub[] }, Server, IncomingMessage>,
     _reply: FastifyReply<Server, IncomingMessage, ServerResponse>
 ) => {
 
     if (action === "create") {
-        const stub = req.body;
-        const matches = (Array.isArray(stub.match) ? stub.match : [stub.match]).map(item => item?.[0] !== "/" ? `/${item}` : item);
-        for (const stubMatch of matches) {
-            req.stubs.set(stubMatch, stub);
+        const stubs = req.body;
+        for (const stub of stubs) {
+            const matches = (Array.isArray(stub.match) ? stub.match : [stub.match]).map(item => item?.[0] !== "/" ? `/${item}` : item);
+            for (const stubMatch of matches) {
+                req.stubs.set(stubMatch, stub);
+            }
         }
 
-        return withResult(req, { created: matches, stub });
+        return withResult(req, { created: stubs.length });
     }
 
     if (action === "getAll") {
